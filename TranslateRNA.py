34fca7e5e83903f72a65895761a39362d7c151d5
Jessica Mattick <jmatty16@users.noreply.github.com>
2017-11-25 17:16:20 -0600

    Create TranslateRNA.py

---
+++ TranslateRNA.py
@@ -0,0 +1,80 @@
+#Translates RNA sequence to protein sequence
+
+codons = {
+"UUU" : "F",
+"CUU" : "L",
+"AUU" : "I",
+"GUU" : "V",
+"UUC" : "F",
+"CUC" : "L",
+"AUC" : "I",
+"GUC" : "V",
+"UUA" : "L",
+"CUA" : "L",
+"AUA" : "I",     
+"GUA" : "V",
+"UUG" : "L",
+"CUG" : "L",
+"AUG" : "M",
+"GUG" : "V",
+"UCU" : "S",
+"CCU" : "P",
+"ACU" : "T",
+"GCU" : "A",
+"UCC" : "S",
+"CCC" : "P",
+"ACC" : "T",
+"GCC" : "A",
+"UCA" : "S",
+"CCA" : "P",
+"ACA" : "T",
+"GCA" : "A",
+"UCG" : "S",
+"CCG" : "P",
+"ACG" : "T",
+"GCG" : "A",
+"UAU" : "Y",
+"CAU" : "H",
+"AAU" : "N",
+"GAU" : "D",
+"UAC" : "Y",
+"CAC" : "H",
+"AAC" : "N",
+"GAC" : "D",
+"UAA" : "Stop",
+"CAA" : "Q",
+"AAA" : "K",
+"GAA" : "E",
+"UAG" : "Stop",
+"CAG" : "Q",
+"AAG" : "K",
+"GAG" : "E",
+"UGU" : "C",
+"CGU" : "R",
+"AGU" : "S",
+"GGU" : "G",
+"UGC" : "C",
+"CGC" : "R",
+"AGC" : "S",
+"GGC" : "G",
+"UGA" : "Stop",
+"CGA" : "R",
+"AGA" : "R",
+"GGA" : "G",
+"UGG" : "W",
+"CGG" : "R",
+"AGG" : "R",
+"GGG" : "G"
+}
+f = open("C:\Users\jmatt\Desktop\Playground\Rosalind\Stronghold\TranslateRNAinput.txt", "r");
+z = open("C:\Users\jmatt\Desktop\Playground\Rosalind\Stronghold\TranslateRNAoutput.txt", "w");
+s = str(f.readline().strip());
+f.close();
+p = "";
+i = 0;
+while i<(len(s)-3):
+    cod = s[i:i+3];
+    p += codons[cod];
+    i+=3;
+z.write(p);
+z.close();
